c37365a206d34daf5c2164fc9b16a291fea7b284
IsrakAhmed <israkahmed7@gmail.com>
2023-03-13 11:51:18 +0600

    Lab Day 4 - done

---
+++ Day 4/script.js
@@ -0,0 +1,35 @@
+
+/*var student = { name:"Israk Ahmed", color:"white", model:"Aventador" }
+
+
+document.getElementById("demo").innerHTML = student.name;*/
+
+/*function oddName(){
+    var i;
+    for(i = 1; i <= 20; i++){
+        if(i % 2 == 1){
+            document.write(i, "<br>");
+        }
+    }
+}
+
+oddName();
+
+var x = customFunction(5,6);
+
+function customFunction(a,b){
+    return a*b;
+}
+
+document.getElementById("demo").innerHTML = x;*/
+
+/*var age = 19;
+
+age >= 18 ? document.write("YES, You can Vote!") : document.write("No, You can not vote.")*/
+
+var i;
+for(i = 1; i <= 100; i++){
+    if(i % 2 == 1){
+        document.write(i, "<br>");
+    }
+}
