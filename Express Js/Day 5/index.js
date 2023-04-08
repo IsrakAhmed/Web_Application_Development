3da3cce59f5b1c64743c01d732eb5d33009e40d1
IsrakAhmed <israkahmed7@gmail.com>
2023-04-08 23:17:56 +0600

    html and css file added

--- Express Js/Day 5/index.js
+++ Express Js/Day 5/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
