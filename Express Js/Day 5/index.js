796205939c7cae977da6dcc45e4b2d433e026368
IsrakAhmed <israkahmed7@gmail.com>
2023-04-08 23:07:02 +0600

    server created day 5

---
+++ Express Js/Day 5/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
