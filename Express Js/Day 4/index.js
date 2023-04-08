ee55cf9f6c394ac28fe39a1cb7b92780ff101a76
IsrakAhmed <israkahmed7@gmail.com>
2023-04-08 22:47:57 +0600

    middleware practice day 4

--- Express Js/Day 4/index.js
+++ Express Js/Day 4/index.js
@@ -1,11 +1,24 @@
 require("dotenv").config();
 const express = require('express');
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
-app.get('/',(req,res) => {
+const myMiddleWare = (req, res, next) => {
+    console.log('middle ware');
+    req.currentTime = new Date(Date.now());
+    next();
+};
+
+//app.use(myMiddleWare);
+
+app.get('/',myMiddleWare,(req,res) => {
+    console.log('hello home' + req.currentTime);
     res.send('<h1>this is home route</h1>');
+});
+
+app.get('/about',myMiddleWare,(req,res) => {
+    console.log('hello about' + req.currentTime);
+    res.send('<h1>this is about route</h1>');
 });
 
 app.listen(PORT, () => {
